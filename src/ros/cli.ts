f115f86ea38b9f0ba8c1b2b1f6ca07d2690af696
Sean Yen <seanyen@microsoft.com>
2019-08-09 16:07:12 -0700

    fix rosrun and roslaunch commands for unfinished invocation (#98)

--- src/ros/cli.ts
+++ src/ros/cli.ts
@@ -23,16 +23,20 @@ async function preparerosrun(): Promise<vscode.Terminal> {
     }), {
         placeHolder: "Choose a package",
     });
-    if (packageName !== undefined) {
-        let basenames = (files: string[]) => files.map((file) => path.basename(file));
-
-        const executables = rosApi.findPackageExecutables(packageName).then(basenames);
-        let target = await vscode.window.showQuickPick(executables, { placeHolder: "Choose an executable" });
-        let argument = await vscode.window.showInputBox({ placeHolder: "Enter any extra arguments" });
-        return rosApi.activateRosrun(packageName, target, argument);
-    } else {
-        // none of the packages selected, error!
+    if (!packageName) {
+        return;
     }
+    let basenames = (files: string[]) => files.map((file) => path.basename(file));
+    const executables = rosApi.findPackageExecutables(packageName).then(basenames);
+    let target = await vscode.window.showQuickPick(executables, { placeHolder: "Choose an executable" });
+    if (!target) {
+        return;
+    }
+    let argument = await vscode.window.showInputBox({ placeHolder: "Enter any extra arguments" });
+    if (argument == undefined) {
+        return;
+    }
+    return rosApi.activateRosrun(packageName, target, argument);
 }
 
 export async function roslaunch(context: vscode.ExtensionContext) {
@@ -50,13 +54,19 @@ async function prepareroslaunch(): Promise<vscode.Terminal> {
     }), {
         placeHolder: "Choose a package",
     });
-    if (packageName !== undefined) {
-        const launchFiles = await rosApi.findPackageLaunchFiles(packageName);
-        const launchFileBasenames = launchFiles.map((filename) => path.basename(filename));
-        let target = await vscode.window.showQuickPick(launchFileBasenames, { placeHolder: "Choose a launch file" });
-        let argument = await vscode.window.showInputBox({ placeHolder: "Enter any extra arguments" });
-        return rosApi.activateRoslaunch(launchFiles[launchFileBasenames.indexOf(target)], argument);
-    } else {
-        // none of the packages selected, error!
+    if (!packageName) {
+        return;
     }
+    const launchFiles = await rosApi.findPackageLaunchFiles(packageName);
+    const launchFileBasenames = launchFiles.map((filename) => path.basename(filename));
+    let target = await vscode.window.showQuickPick(launchFileBasenames, { placeHolder: "Choose a launch file" });
+    const launchFilePath = launchFiles[launchFileBasenames.indexOf(target)];
+    if (!launchFilePath) {
+        return;
+    }
+    let argument = await vscode.window.showInputBox({ placeHolder: "Enter any extra arguments" });
+    if (argument == undefined) {
+        return;
+    }
+    return rosApi.activateRoslaunch(launchFilePath, argument);
 }
