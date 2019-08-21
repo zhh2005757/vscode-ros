b15053763bafbd0836d5e489e91343ca833fd371
James Xu <kejia.xu@microsoft.com>
2019-08-20 17:27:43 -0700

    update copyright header (#107)

--- src/ros/ros2/daemon.ts
+++ src/ros/ros2/daemon.ts
@@ -1,4 +1,4 @@
-// Copyright (c) Andrew Short. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 import * as child_process from "child_process";
